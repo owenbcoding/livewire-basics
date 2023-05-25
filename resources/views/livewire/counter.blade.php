4274dc107a58bc7097abdef59d542f966599ce10
owenbcoding <eoghankb@gmail.com>
2023-05-25 05:47:28 +0100

    inital commmit

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+    <span>2</span>
+    <button wire:click="decrement">-</button>
+    <button wire:click="increment">+</button>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <livewire:styles />
+</head>
+
+<body>
+    <livewire:counter />
+    <livewire:scripts />
+</body>
+
+</html>
